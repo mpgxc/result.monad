484b112433f540870bc7869ce09328223d4a6ea6
Mateus Garcia <mpgx5.c@gmail.com>
2023-10-30 01:59:46 -0300

    refactor(*): rewrite methods

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,33 @@
-type Success<F, S> = {
-  value: S;
-  hasError: false;
+type Ok<T> = {
+  value: T;
+  isOk: true;
 };
 
-type Failure<F, S> = {
-  value: F;
-  hasError: true;
+type Err<T> = {
+  value: T;
+  isOk: false;
 };
 
-type Either<F, S> = Failure<F, never> | Success<never, S>;
+type Notifications<T> = Array<Err<T>>;
 
-type Notifications<T> = Array<Failure<T, never>>;
+type Result<T, E> = Err<E> | Ok<T>;
 
-class Result {
-  static combine<T>(notifications: Notifications<T>): Failure<T[], never> {
-    return this.failure(notifications.map(({ value }) => value));
-  }
+const combine = <T>(notifications: Notifications<T>): Err<T[]> =>
+  Err(notifications.map(({ value }) => value));
 
-  static failure<T>(value: T): Failure<T, never> {
-    return {
-      value,
-      hasError: true,
-    };
-  }
+const Err = <T>(value: T): Err<T> => ({
+  value,
+  isOk: false,
+});
 
-  static success<T>(value?: T): Success<never, T> {
-    return {
-      value: value as T,
-      hasError: false,
-    };
-  }
-}
+const Ok = <T>(value?: T): Ok<T> => ({
+  value,
+  isOk: true,
+});
 
-export { Either, Failure, Notifications, Result, Success };
+const Result = {
+  Ok,
+  Err,
+};
+
+export { Result, Ok, Err, Notifications };
